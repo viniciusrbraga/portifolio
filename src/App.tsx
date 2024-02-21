1d9e6e8ca174b6de7d14d3754736e496e400f5cf
Vinicius Braga <viniciusrb@gmail.com>
2024-02-21 19:16:56 -0300

    finalização do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,31 @@
-import React from 'react'
+import { ThemeProvider } from 'styled-components'
+
 import EstiloGlobal, { Container } from './styles'
 import Sidebar from './containers/sidebar'
 import Sobre from './Sobre'
 import Projetos from './Projetos'
+import temaLight from './themes/light'
+import temaDark from './themes/dark'
+import { useState } from 'react'
 
 function App() {
+  const [estausandoTemaDark, setEstausandoTemaDark] = useState(false)
+
+  function trocaTema() {
+    setEstausandoTemaDark(!estausandoTemaDark)
+  }
+
   return (
-    <>
+    <ThemeProvider theme={estausandoTemaDark ? temaDark : temaLight}>
       <EstiloGlobal />
       <Container>
-        <Sidebar />
+        <Sidebar trocaTema={trocaTema} />
         <main>
           <Sobre />
           <Projetos />
         </main>
       </Container>
-    </>
+    </ThemeProvider>
   )
 }
 
--- src/components/Projeto/styles.ts
+++ src/components/Projeto/styles.ts
@@ -1,12 +1,12 @@
 import styled from 'styled-components'
 
 export const Card = styled.div`
-  border: 1px solid #c1c1c1;
+  border: 1px solid ${(props) => props.theme.corDaBorda};
   padding: 16px;
 `
 export const LinkBotao = styled.a`
-  color: #fff;
-  background-color: #4476bf;
+  color: ${(props) => props.theme.corDeFundo};
+  background-color: ${(props) => props.theme.corDeFundoBotao};
   font-size: 14px;
   text-decoration: none;
   padding: 8px;
